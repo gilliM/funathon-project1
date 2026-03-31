9b75cd578bcdf02c6beb5aefd63950da6cc97cda
wzsa9j <nicolas.toulemonde@insee.fr>
2026-03-31 13:05:26 +0000

    main - steps 1 to 2

--- intermediate_solutions/solution_step1_to_2/main.py
+++ intermediate_solutions/solution_step1_to_2/main.py
@@ -1,251 +1,150 @@
 # %%
+# ============================================
+# STEP 1 — Generate synthetic regression data
+# ============================================
+
+import polars as pl
 import pandas as pd
-from sklearn.compose import make_column_selector as selector
-from sklearn.compose import make_column_transformer
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, HistGradientBoostingRegressor
-from sklearn.model_selection import cross_validate, train_test_split, GridSearchCV
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
-from sklearn.metrics import mean_absolute_percentage_error, r2_score
-from sklearn.dummy import DummyRegressor
-import time
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
+from sklearn.compose import ColumnTransformer, TransformedTargetRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
 
-from plotnine import (
-    ggplot, aes, geom_bar, geom_point, facet_grid,
-    labs, theme, element_text, position_dodge
-)
+RANDOM_STATE=202605
+
+trans = pl.concat(
+    [
+        pl.read_parquet('s3://confpns/synthetic-transactions/rawdata/transactions/transactions_houses_final.parquet'),
+        pl.read_parquet('s3://confpns/synthetic-transactions/rawdata/transactions/transactions_flats_final.parquet')
+    ]
+    ).to_pandas()
+
+trans = trans[trans["ccodep"].isin(["75", "77", "78", "91", "92", "93", "94", "95"])]
+trans["valfonc_m2"] = trans["valeurfonc"] / trans["dsupdc"]
+
+
+
 # %%
-data = pd.read_parquet('s3://confpns/synthetic-transactions/rawdata/transactions/transactions_flats_final.parquet')
-data_h = pd.read_parquet("s3://confpns/synthetic-transactions/rawdata/transactions/transactions_houses_final.parquet")
+# ============================================
+# STEP 2 — Preprocessing
+#   - Outlier removal
+#   - Scaling
+#   - One-hot encoding
+# ============================================
+# Apply some deterministic threshold on the dataframe
+trans = trans[(trans["valfonc_m2"] < 200000) & (trans["valfonc_m2"] > 100)]
 
-# %%
-if data.columns.all() == data_h.columns.all():
-    data_all = pd.concat([data, data_h])
+# Apply IQR methods for the outlier removal
+def outlier_transform(y, lower=0.1, upper=0.9):
+    """
+    Transform Y target to log(Y) and remove outliers with IQR method
 
-# Setting data type of dteloc to a more meaningful category 
-data_all["dteloc"] = pd.Categorical(
-    data_all["dteloc"],
+    Args :
+        y : target
+        lower: lower quantile for the IQR
+        upper: upper quantile for the IQR
+    """
+    Q_lower = np.quantile(y, lower)
+    Q_upper = np.quantile(y, upper)
+    IQR = Q_upper - Q_lower
+
+    mask = (y >= Q_lower - 1.5 * IQR) & (y <= Q_upper + 1.5 * IQR)
+    return mask
+
+mask = outlier_transform(trans["valfonc_m2"])
+trans = trans[mask].reset_index(drop=True)
+
+trans = trans.dropna(subset = "valfonc_m2")
+df = trans.drop(columns=[
+    'idmutation', "idnatmut", "libnatmut", 
+    "valeurfonc", "ccodep", "depcom", "distance_ltm", "distance_ltm_corr"
+])
+df = df.dropna()
+df["dteloc"] = pd.Categorical(
+    df["dteloc"],
     categories=["1", "2"],
-    ordered=False  # Set to True if the categories have a meaningful order
+    ordered=False
 ).rename_categories({"1": "House", "2": "Flat"})
 
-data_all["price_sqm"] = data_all["valeurfonc"] / data_all["dsupdc"]
-data_all.loc[data_all["dnbniv"]==0, "dnivrel"] = 0
-data_all.loc[data_all["dnbniv"]>0, "dnivrel"] = data_all["dniv"] / data_all["dnbniv"]
+df['jannath_10'] = (df['jannath'] // 10)*10
+df['jannath_10'] = df['jannath_10'].where(df['jannath_10'] >= 1850, 1840)
 
-# Selecting the only cols we want to use
+# Dropping old column
+df = df.drop(columns=["jannath"])
 
-# %%
-# Sample some data
-features_list = [
-    ["depcom", "x", "y", "dteloc", "dnbppr", "dnbcha", "dsupdc"],
-    ['anneemut', 'dteloc', 'jannath', 'ccodep', 'depcom', 'x', 'y','distance_ltm', 'dnbniv', 'dnbbai', 'dnbdou',
-       'dnblav', 'dnbwc', 'dnbppr', 'dnbsam', 'dnbcha', 'dnbcu8', 'dnbcu9',
-       'dnbsea', 'dnbann', 'dnbpdc', 'dsupdc', 'dniv', 'nb_terrasses',
-       'nb_greniers', 'nb_caves', 'nb_autresdep'], 
-    ['anneemut', 'dteloc', 'jannath', 'ccodep', 'depcom', 'x', 'y','distance_ltm', 'dnbniv', 'dnbbai', 'dnbdou',
-       'dnblav', 'dnbwc', 'dnbppr', 'dnbsam', 'dnbcha', 'dnbcu8', 'dnbcu9',
-       'dnbsea', 'dnbann', 'dnbpdc', 'dsupdc', 'dniv', 'nb_terrasses',
-       'nb_greniers', 'nb_caves', 'nb_autresdep', 'dnivrel']
-] 
-target = "price_sqm"
-data_75 = data_all[data_all['ccodep'] == '75'].dropna()
-# %%
-max_target = data_75[target].quantile(0.9)
-min_target = data_75[target].quantile(0.1)
-data_preproc = data_75[(data_75[target] <= max_target) & (data_75[target] >= min_target)]
 
-# %%
-# Encoding issue - differentiate between numeric and other
-cols_cat = selector(dtype_exclude="number")
-cols_num = selector(dtype_include="number")
+def date_to_days(X: pd.Series, ref_date:pd.Timestamp):
+    # converts a date to a difference to ref_date : 
+    diff_dt = pd.to_datetime(X) - ref_date
+    # Extract days part from datetime object
+    diff_dt = diff_dt.dt.days
+    # Transform it from a Pandas series to a Numpy nd array, used by scikit learn for input
+    diff_dt = diff_dt.to_numpy().reshape(-1, 1)
 
-# %%
-X = data_preproc[features_list[1]]
-y = data_preproc[target] # depcom (question encoding ?), dteloc (boolean apt), dnbppr, dnbcha, dsupdc
-X.hist(bins=10)
-# Features are not troncated
-# %%
-X.hist(log=True)
-# %%
-y.hist(bins=10)  # skewed to 0 bcs prices
-# %%
-y.plot(kind='hist', logx=True, logy=True)  # sharp decrease for assets above 1Me
+    return diff_dt 
+    
+date_transformer = FunctionTransformer(
+    date_to_days,
+    kw_args={"ref_date": pd.Timestamp('2010-01-01 00:00')}
+    )
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("cat", OneHotEncoder(handle_unknown="ignore"), ["dteloc", "jannath_10"]),  # one-hot encoder on feature
+        ("dat", date_transformer, "datemut") # feature time since 01-01-2010
+    ],
+    remainder="passthrough"  # to keep features not transformed
+) 
+
+
+def log_transform(y):
+    return np.log10(y)
+
+def inverse_log_transform(y):
+    return 10 ** y
+
+y_transformer = FunctionTransformer(
+    func = log_transform,
+    inverse_func = inverse_log_transform)
 
 # %%
-preprocessor = make_column_transformer(
-    (OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1), cols_cat),
-    remainder="passthrough",
+# ============================================
+# STEP 3 — Train / test split, model fitting,
+#          and performance evaluation
+# ============================================
+
+# Split features / target
+X = df.drop(columns="valfonc_m2")  # X must contain only the features we'll learn from
+y = df["valfonc_m2"]  # target must be a dataframe with 1 column
+
+# Split train / test set
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=RANDOM_STATE
 )
-model_gb = make_pipeline(preprocessor, GradientBoostingRegressor())
-model_hist = make_pipeline(preprocessor, HistGradientBoostingRegressor())
-model_rf = make_pipeline(preprocessor, RandomForestRegressor())
 
-models_list = [model_gb, model_hist, model_rf]
-# %%
-# Impact of tails of price_sqm on model performance
-## Option 1 - remove extreme values
-# 1a - thresholds 0.1/0.9
-max_target = data_75[target].quantile(0.9)
-min_target = data_75[target].quantile(0.1)
-data_preproc = data_75[(data_75[target] <= max_target) & (data_75[target] >= min_target)]
 
-# 1b - thresholds 0.2/0.8
-# max_target = data_75[target].quantile(0.8)
-# min_target = data_75[target].quantile(0.2)
-# data_preproc = data_75[(data_75[target] <= max_target) & (data_75[target] >= min_target)]
-# %%
-#| label: nb_features_accuracy
-# Impact of nb of features on accuracy :  
-
-scoring = {
-    'r2': 'r2',
-    'mape': 'neg_mean_absolute_percentage_error'
+rf_params = {
+    "n_estimators": 100,
+    "max_depth": 5,
+    "max_features": "sqrt",
+    "min_samples_split": 2,
+    "min_samples_leaf": 10,
+    "random_state": RANDOM_STATE,
+    "oob_score": True,
+    "n_jobs": -1,  # The number of jobs to run in parallel, -1 using all processors
 }
 
-for feature in features_list:
-    X = data_preproc[feature]
-    y = data_preproc[target] # depcom (question encoding ?), dteloc (boolean apt), dnbppr, dnbcha, dsupdc
+rf_pipeline = Pipeline([
+    ('preprocessing', preprocessor),
+    ('RF', RandomForestRegressor(**rf_params))
+])
 
-    start = time.time()
-    cv_results = cross_validate(estimator=model_hist, X=X, y=y, scoring=scoring, return_train_score=True, cv=5)
-    elapsed_time = time.time() - start
-    
-    print(
-        f"Data set : \n"
-        f"   - {X.shape[1]} col ({list(X.columns)})\n"
-        f"   - n_ops : {X.shape[0]} \n"
-        f"Training score is : \n"
-        f"   - with R² : {cv_results['train_r2'].mean():.3f} +- {cv_results['train_r2'].std():.3f} \n"
-        f"   - with MAPE : {-cv_results['train_mape'].mean():.3f} +- {cv_results['train_mape'].std():.3f} \n"
-        f"Mean cross validation accuracy is : \n"
-        f"   - with R² : {cv_results['test_r2'].mean():.3f} +- {cv_results['test_r2'].std():.3f} \n"
-        f"   - with MAPE : {-cv_results['test_mape'].mean():.3f} +- {cv_results['test_mape'].std():.3f} \n"
-        f" with an elapsed time of {elapsed_time:.3f}s"
-    )
-
-# %%
-# features and model 
-#| label: features_plus_model_selection 
-max_target = data_75[target].quantile(0.9)
-min_target = data_75[target].quantile(0.1)
-data_preproc = data_75[(data_75[target] <= max_target) & (data_75[target] >= min_target)]
-
-for feature in features_list:
-    X = data_preproc[feature]
-    y = data_preproc[target] # depcom (question encoding ?), dteloc (boolean apt), dnbppr, dnbcha, dsupdc
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-    print(
-        f"Data set : \n"
-        f"   - {X.shape[1]} col ({list(X.columns)})\n"
-        f"   - n_ops : {X.shape[0]} \n"
-    )
-
-    for model in models_list:
-        start = time.time()
-        model.fit(X_train, y_train)
-        elapsed_time = time.time() - start
-        y_pred = model.predict(X_test)
-        mape = mean_absolute_percentage_error(y_test, y_pred)
-        rtwo = r2_score(y_test, y_pred)
-
-        print(
-            f"= Model is : {model.steps[-1][1].__class__.__name__} \n"
-            f"  Metrics are : \n"
-            f"   - MAPE : {mape:.3f} \n"
-            f"   - R2 : {rtwo:.3f} \n"
-            f"  with an elapsed time of {elapsed_time:.3f}s"
-        )
-
-# %%
-#| label: hyperparams_tuning
-dummy_regr = DummyRegressor(strategy="mean")
-dummy_regr.fit(X, y)
-dummy_regr.score(X, y)
-# %%
-
-max_target = data_75[target].quantile(0.9)
-min_target = data_75[target].quantile(0.1)
-data_preproc = data_75[(data_75[target] <= max_target) & (data_75[target] >= min_target)]
-
-X = data_preproc[features_list[1]]
-y = data_preproc[target]
-
-# Hyper parameters tuning
-param_grid =   {
-    'histgradientboostingregressor__learning_rate': [0.1, 0.5, 0.8], 
-    'histgradientboostingregressor__max_iter': [50, 100, 250],
-    'histgradientboostingregressor__l2_regularization': [0, 0.01, 0.5],
-    'histgradientboostingregressor__max_features': [0.65, 0.75, 0.85]
-  }
-
-gs = GridSearchCV(
-    model_hist, 
-    param_grid, 
-    scoring={'neg_mape':'neg_mean_absolute_percentage_error', 'r2':'r2'},
-    refit='neg_mape')
-
-gs.fit(X,y)
-print(
-    f"Best estimator has params : {gs.best_params_} \n associated with a MAPE of {-gs.best_score_}"
+model = TransformedTargetRegressor(
+    regressor=rf_pipeline,
+    transformer=y_transformer
 )
-
-
-# %%
-#| label: hyperparams_plot
-# Extract results
-results = pd.DataFrame(gs.cv_results_)[["param_histgradientboostingregressor__learning_rate",
-                     "param_histgradientboostingregressor__max_iter",
-                     "param_histgradientboostingregressor__max_features",
-                     "param_histgradientboostingregressor__l2_regularization",
-                     "mean_test_neg_mape",
-                     "mean_test_r2"]]
-plot_data = results.rename(columns={
-    "param_histgradientboostingregressor__learning_rate": "learning_rate",
-    "param_histgradientboostingregressor__max_iter": "max_iter",
-    "param_histgradientboostingregressor__max_features": "max_features",
-    "param_histgradientboostingregressor__l2_regularization": "l2",
-    "mean_test_neg_mape": "neg_mape", 
-    "mean_test_r2": "r2"
-}).dropna()
-plot_data["mape"] = -plot_data["neg_mape"]*4  # Scale MAPE to align with R²
-plot_data["learning_rate"] = plot_data["learning_rate"].astype(str)
-plot_data["max_iter"] = plot_data["max_iter"].astype(str)
-plot_data["max_features"] = plot_data["max_features"].astype(str)
-plot_data["l2"] = plot_data["l2"].astype(str)
-
-# Create the plot
-(
-    ggplot() +
-    geom_bar(
-        aes(x="max_iter", y="mape", fill="l2"),
-        data=plot_data,
-        stat="identity",
-        position="dodge",
-        alpha=0.7
-    ) +
-    geom_point(
-        aes(x="max_iter", y="r2", color="l2"),
-        data=plot_data,
-        position=position_dodge(width=1),
-        size=3,
-        shape="o"
-    ) +
-    facet_grid("max_features ~ learning_rate", labeller="label_both") +
-    labs(
-        title="Hyperparameter Tuning: 4xMAPE (Bars) and R² (Points)",
-        x="Max Iterations",
-        y="Scaled Metrics (4xMAPE or R²)",
-        fill="L2 Regularization",
-        color="L2 Regularization",
-        caption="Bars: 4xMAPE (%). Points: R²."
-    ) +
-    theme_matplotlib() +
-    theme(
-        plot_caption=element_text(hjust=0, size=10, margin={"t": 10}),
-        figure_size=(12, 10),
-        plot_margin=0.1
-    )
-)
-# %%
